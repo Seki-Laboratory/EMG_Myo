ef47181f6068fa4dc15682d673596e81f672eaf5
usui9617 <usui.tomoyasu@astina.co>
2020-10-25 19:04:28 +0900

    myo_emg_only

---
+++ emg.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import collections
+import myo
+import time
+import sys
+
+
+class Emg(myo.DeviceListener):
+
+  def on_arm_synced(self, event):
+    event.device.stream_emg(False)
+
+  def on_emg(self, event):
+    emg = event.emg
+    #print(emg)
+
+
+
+def main():
+  myo.init(sdk_path=r'C:\work\myo-sdk-win-0.9.0-main')
+  hub = myo.Hub()
+  listener = Emg()
+  while hub.run(listener.on_event, 500):
+    print("a")
+    pass
+
+if __name__ == '__main__':
+  main()
