cbaf387b72b2937f89fb1b9d3e5bae7f54a9cae4
usui9617 <usui.tomoyasu@astina.co>
2020-11-01 01:45:22 +0900

    Moving and Normal RMS get commit

--- emg.py
+++ emg.py
@@ -6,69 +6,81 @@ import time
 import sys
 import numpy as np
 from numpy.core.fromnumeric import reshape
+import csv
 
 #Emgクラス　サンプリング周波数200でデータを取得するクラス
 class Emg(myo.DeviceListener):
 
-  def __init__(self):
-    print("class Emg instanced")
+  def __init__(self,mode):
+    print("class Emg instanced mode=",mode)
     self.rms = np.zeros((1,8))   
-    self.result = np.zeros((1,8)) 
     self.add =  np.zeros((1,8)) 
+    self.mode = mode
     self.i = 0
+    self.j = 0
     
 
   def on_connected(self, event):
-    event.device.stream_emg(True)
- 
-
+      event.device.stream_emg(True)
 
   def on_emg(self,event):
     self.emg = np.array(event.emg)**2
-    self.emg = np.reshape(self.emg,(1,8))
-    
-    if self.add.shape[0] <= 21:
-      self.add = np.append(self.add,self.emg,axis=0)
-    else:
-      self.add = np.delete(self.add, 1, 0)
-
-    sum = np.sum(self.add[1:],axis=0)
-    ave = sum/20
-    sqrt = np.sqrt(ave)
-    np.set_printoptions(precision=0)
-    print(sqrt)
+#______________Moving_RMS_________________________
+    if self.mode == 0:
+      self.emg = np.array(event.emg)**2
+      self.emg = np.reshape(self.emg,(1,8))
       
+      if self.add.shape[0] <= 21:
+        self.add = np.append(self.add,self.emg,axis=0)
+      else:
+        self.add = np.delete(self.add, 1, 0)
 
-  
-  # def on_emg(self,event):
-  #   self.emg = event.emg
-  #   square = np.array(self.emg)**2
-  #   self.rms += square
-    
+      sum = np.sum(self.add[1:],axis=0)
+      ave = sum/20
+      sqrt = np.sqrt(ave)
+      sqrt = np.round(sqrt, decimals=2)
+      print(sqrt)
 
-  #   if self.i%20 == 0:
-  #     print(self.rms/20)
-  #     self.i = 0
-  #   self.i = self.i+1
+      if self.i <= 19:
+        with open('MRMSdata.csv', 'a') as f:
+          writer = csv.writer(f, lineterminator='\n') # 行末は改行
+          writer.writerow(sqrt)
+        self.i += 1
 
-    
-    
+#___________________RMS_____________________________
+    elif self.mode == 1:
+      self.rms += self.emg
 
-  #   self.rms = np.zeros(8,dtype = int)   
-  #   self.result = np.zeros(8,dtype = int)
+      if self.j == 19:
+        ave = self.rms/20
+        sqrt = np.sqrt(ave)
+        sqrt = np.round(sqrt, decimals=2)
+        sqrt = np.reshape(sqrt,(8))
+        print(sqrt)
+
+        if self.i <= 19:
+          with open('RMSdata.csv', 'a') as f:
+            writer = csv.writer(f, lineterminator='\n') # 行末は改行
+            writer.writerow(sqrt)
+          self.i += 1
+
+        self.rms = np.zeros((1,8))
+        self.j = 0
+
+      self.j += 1
 
 #main関数
 def main():
   myo.init(sdk_path=r'C:\work\myo-sdk-win-0.9.0-main')
   hub = myo.Hub()  #myoモジュールのHubクラスのインスタンス
-  listener = Emg() #emgクラスのインスタンス
+  listener = Emg(mode=1) #emgクラスのインスタンス
 
   try:
     start = time.time()
-    while hub.run(listener.on_event, 500):
+    while hub.run(listener.on_event, 100):
       current = time.time()
       t = float(current - start)
-      if t >= 1:
+      if listener.i >= 20:
         print("stop"  ,t,"秒")
         break
 
--- list_test2.py
+++
@@ -1,6 +0,0 @@
-import functools, operator
-
-list = [[0,1,2,3,4,5], [0,1,2,3,4,5], [0,1,2,3,4,5]]
-new_list = [functools.reduce(operator.add, x) for x in zip(*list)]
-
-print(new_list)
