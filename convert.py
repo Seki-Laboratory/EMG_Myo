0541e94b065f2344ca735742491997b8ef2745d8
usui9617 <usui.tomoyasu@astina.co>
2021-12-08 14:42:57 +0900

    1208_1

--- convert.py
+++ convert.py
@@ -7,12 +7,12 @@ LOAD_DATA_DIR_PATH = "C:/Users/usui0/Desktop/emg_data"
 SAVE_DATA_DIR_PATH = "C:/Users/usui0/Desktop/data/"+JES
 
 
-for i in range(100,10100,100):
+for i in range(1000,10100,1):
     CSVList=np.loadtxt('C:/Users/usui0/Desktop/emg_data/'+JES+'/'+JES+'_emgdata0.csv', delimiter=',')
     # img_gray = np.array(RMSList [:,0:8], dtype = np.uint8)
-    img1 = np.array(CSVList [i-100:i,:],dtype = np.float)
+    img1 = np.array(CSVList [i-1000:i,:],dtype = np.float)
     img  = img1*1000
-    np.savetxt(SAVE_DATA_DIR_PATH+"/"+str(i-100)+'.csv', img, delimiter=',', fmt='%d')
+    np.savetxt(SAVE_DATA_DIR_PATH+"/"+str(i-1000)+'.csv', img, delimiter=',', fmt='%d')
     print(i)
 
 
